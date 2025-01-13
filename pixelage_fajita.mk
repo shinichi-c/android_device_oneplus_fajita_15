ce72840f23e3b589bb18acaf8eca5739c03bc7b6
shinichi-c <gokus12456@gmail.com>
2025-01-13 10:17:02 +0000

    fajita: bringup pixelage

--- pixelage_fajita.mk
+++ pixelage_fajita.mk
@@ -22,9 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_fajita
+# pixelage Stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+PRODUCT_NAME := pixelage_fajita
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
