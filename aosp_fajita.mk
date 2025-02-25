3b9554bfadcc48329230af5d3ad482558410c940
shinichi-c <gokus12456@gmail.com>
2025-02-25 06:26:22 +0000

    fajita: bringup aosp

--- aosp_fajita.mk
+++ aosp_fajita.mk
@@ -21,10 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fajita device
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixelos stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_fajita
+# Pixelos stuff
+PRODUCT_NAME := aosp_fajita
+TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
