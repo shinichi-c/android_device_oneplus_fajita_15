f363870483da6644511be8acc35b898035f47aab
shinichi-c <gokus12456@gmail.com>
2025-03-08 10:31:20 +0000

    fajita: add evolution_x flags

--- lineage_fajita.mk
+++ lineage_fajita.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/oneplus/fajita/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+WITH_GMS := true
+BUILD_BCR := true
+TARGET_HAS_UDFPS := true
 PRODUCT_NAME := lineage_fajita
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
