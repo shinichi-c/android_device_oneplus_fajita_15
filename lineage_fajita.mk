5e11727f639af7d96f614856acd14ba962e686ae
kudo <Gokus12456@gmail.com>
2025-03-08 20:41:38 +0530

    Update lineage_fajita.mk

--- lineage_fajita.mk
+++ lineage_fajita.mk
@@ -24,7 +24,7 @@ $(call inherit-product, device/oneplus/fajita/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-TARGET_USES_PICO_GAPPS := true
+WITH_GMS := true
 BUILD_BCR := true
 PRODUCT_NAME := lineage_fajita
 PRODUCT_DEVICE := fajita
