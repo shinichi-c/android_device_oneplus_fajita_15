80f3c82611be7f616a4d6086b0589668da7cbc8f
kudo <Gokus12456@gmail.com>
2025-08-20 04:28:43 +0530

    fajita: move to full gapps

--- lineage_fajita.mk
+++ lineage_fajita.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_DISABLE_EPPE := true
 WITH_BCR := true
-TARGET_USES_OMNI_GAPPS :=true
+WITH_GMS := true
 TARGET_OPTIMIZED_DEXOPT := true
 PRODUCT_NAME := lineage_fajita
 PRODUCT_DEVICE := fajita
