ccfe6a969dc4f6bf294533c6532c721af5bfddbf
shinichi-c <gokus12456@gmail.com>
2026-02-09 11:09:32 +0000

    Bringup: EvoX

--- lineage_fajita.mk
+++ lineage_fajita.mk
@@ -25,6 +25,10 @@ $(call inherit-product, device/oneplus/fajita/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_DISABLE_EPPE := true
+WITH_GMS := true
+EVO_BUILD_TYPE := Unofficial
+TARGET_HAS_UDFPS := true
 PRODUCT_NAME := lineage_fajita
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
